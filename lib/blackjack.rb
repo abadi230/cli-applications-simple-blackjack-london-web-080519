f4ae27e17d81b2b3e2c14d8163d73014e627b627
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-24 11:42:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,8 +31,8 @@ end
 def initial_round
   first = deal_card
   second = deal_card
-  total = display_card_total(first + second)
-  return total
+  # total = display_card_total(first + second)
+  return total = first + second
 end
 
 def hit?
