5d8932e31aceb75946cfa14fb6aaaa90d72d9c6b
Abdullah Bajaman <ambajaman@gmail.com>
2019-07-24 11:46:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,7 +33,7 @@ def initial_round
   second = deal_card
   # total = display_card_total(first + second)
   total = first + second
-  puts display_card_total(total.to_s)
+  puts display_card_total(total)
   return total 
   
 end
